c31b973aee00b5b8ccd816d962185d65ef90118a
manishac2002 <manishaac2002@gmail.com>
2025-03-11 17:09:59 +0530

    chore--

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const application = express();
 application.use(express.json());
 
-const port = 4000;
+const port = process.env.PORT || 4000;
+
+application.get('/test', function (req, res) {
+  res.send('Hello World');
+});
 
 application.listen(port, () => {
   console.log(`Server is running on port ${port}`);
